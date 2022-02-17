a9617b08d5dc14ec1cfa0f4148156a53e21a38d6
Markhor <markhor@pop-os.localdomain>
2022-02-17 13:04:22 +0500

    Now, It's good because it has eaten so much of my time

--- src/components/Team/Team3.js
+++ src/components/Team/Team3.js
@@ -5,12 +5,11 @@ import faceOverlay from '../../images/face_overlay.png'
 
 function Team3() {
 
-    return <>
-        <div className="team">
-          {TeamData.map((coursesData, index) => {
-            return (    
-                
-              <div className="face">
+  return <>
+    <div className="team">
+      {TeamData.map((coursesData, index) => {
+        return (
+          <div className="face">
                 <div className="face_pictures">
                     <div className="face_overlay">
                       <img src={faceOverlay} alt="" />
@@ -23,22 +22,26 @@ function Team3() {
                     <i class="fab fa-linkedin"></i>
 
 </div>
-                  </div>
+                  </div><div className="face_image">
+
                   <img src={coursesData.img} alt="" />
-                
+                  </div>
+                <div className="face_text">
+
                     <h5>{coursesData.name}</h5>
                     <h6>{coursesData.job}</h6>
+                </div>
               
                 </div>
               </div>
-                
-              
-              
-        
-            );
-          })}
-        </div>
-    </>;
+
+
+
+
+        );
+      })}
+    </div>
+  </>;
 }
 
 export default Team3;
